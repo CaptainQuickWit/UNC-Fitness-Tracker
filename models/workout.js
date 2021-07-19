2e48d3f05f42038d60d353367c4c1862d1682043
Chetan Patel <Chetan@Chetans-MacBook-Air.local>
2021-07-19 06:18:14 -0400

    added model folder and schema

---
+++ models/workout.js
@@ -0,0 +1,57 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+/**
+ * This is the schema for the web application. It utilizes mongoDB with mongoose rather than the 
+ * traditional mySQL
+ */
+const theSchema = new Schema(
+{
+ 
+    day: {
+    type: Date,
+    default: Date.now
+  },
+
+  exercises: [
+
+    {
+    name : {
+        type: String,
+        trim : true,
+        required : "Enter Exercise name"
+      },
+      type : {
+        type : String,
+        enum: ["resistance", "cardio"],
+        required : "Enter an Exercise type, either resistance or cardio"
+      },
+
+      distance : {
+        type : Number
+      },
+
+      duration : {
+        type : Number,
+        required : "how long the exercise was"
+      },
+
+      weight: {
+        type : Number
+      },
+
+      sets: {
+        type : Number
+      },
+
+      reps: {
+        type : Number
+      }
+
+    }
+
+  ]
+});
+// making a model
+const Workout = mongoose.model("exerciseDB", theSchema);
+//export
+module.exports = exerciseDB;
--- server.js
+++ server.js
@@ -10,13 +10,15 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
+//uses mongoose for the database
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
 
-// routes
-app.use(require("./routes/api.js"));
+// routes that will be used 
+app.use(require("./routes/apiRouting.js"));
+app.use(require("./routes/htmlRouting.js"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
